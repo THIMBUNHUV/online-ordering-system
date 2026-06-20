ec593b367da6369d41d005bef0a3237911b55188
THIMBUNHUV <bunhov1232@gmail.com>
2026-06-20 11:56:14 +0700

    remove reload

--- main.py
+++ main.py
@@ -54,5 +54,5 @@ async def serve_frontend():
 # ── Run ───────────────────────────────────────────────────────────────────
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
     
